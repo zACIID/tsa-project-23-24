af5f3c57e987b30bc7dd3be228ef08f5d6f7fb5c
Pierluigi Marchioro <pierluigi.marchioro@moku.io>
2024-08-30 14:16:07 +0200

    Minor fixes and refactoring to eda.py

--- src/eda.py
+++ src/eda.py
@@ -1,30 +1,13 @@
 import abc
+import pathlib
 
-import matplotlib.pyplot as plt
-import seaborn as sns
 import numpy as np
 import pandas as pd
-import pandas.plotting as pd_plt
-import pmdarima as pm
-import pmdarima.model_selection as pm_modsel
 import statsmodels.tsa.stattools as tsa
-import statsmodels.graphics.tsaplots as tsa_plt
-import statsmodels.tsa.arima_model as arima
-import statsmodels.tsa.seasonal as tsa_season
-import sklearn.model_selection as sk_modsel
-import sklearn.metrics as sk_metrics
-from statsmodels.tsa.statespace.sarimax import SARIMAX
-from statsmodels.tsa.forecasting.stl import STLForecast, STLForecastResults
 
-import visualization as vis
+import src.visualization as vis
 
-# TODO
-"""
-- an abstract class that loads a time series and exposes the original, diffed, log, logdiffed dataframes as properties
-    - the differencing specified via __init__ param
-    - has an abstract method "load_data" that loads the time series -> here I hardcode datasets in the child class
-    - has a "plot_visualization" method that calls all the stuff needed from visualization.py
-"""
+DATA_DIR = pathlib.Path(__file__).parent.resolve() / ".." / "data"
 
 
 class TimeSeries(abc.ABC):
@@ -37,30 +20,49 @@ class TimeSeries(abc.ABC):
         self.log_diffed: pd.Series | None = None
 
     @abc.abstractmethod
-    def load_data(self):
+    def load_data(self, fraction: float = 0.8, left_side: bool = True):
         """
         Load the dataset and populate the time series fields
+
+        :param fraction: fraction of the time series to load
+        :param left_side: True to load the left side of the data, i.e. from the beginning,
+            False to take the portion from the end. This is useful to determine whether to load
+            the train or test data
         """
         pass
 
-    def plot_visualizations(self):
+    def plot_visualizations(
+            self,
+            original: bool = False,
+            diffed: bool = False,
+            log: bool = False,
+            log_diffed: bool = False
+    ):
         # TODO maybe add a "path" param to save the images if provided
-        self._plot_ts_visulizations(self.original, ts_name="Original")
-        self._plot_ts_visulizations(self.diffed, ts_name=f"{self.differencing_periods}-Differenced")
-        self._plot_ts_visulizations(self.original, ts_name="Log")
-        self._plot_ts_visulizations(self.original, ts_name=f"Log {self.differencing_periods}-Differenced")
+        if original:
+            self._plot_ts_visulizations(self.original, ts_name="Original")
+        if diffed:
+            self._plot_ts_visulizations(self.diffed, ts_name=f"{self.differencing_periods}-Differenced")
+        if log:
+            self._plot_ts_visulizations(self.log, ts_name="Log")
+        if log_diffed:
+            self._plot_ts_visulizations(self.log_diffed, ts_name=f"Log {self.differencing_periods}-Differenced")
 
     def _plot_ts_visulizations(self, ts: pd.Series, ts_name: str):
+        # NOTE: fig.show()  # figure is non-interactive and thus cannot be shown
+        # assigning plots to fig because otherwise plot may be shown two times
+
         fig = vis.plot_ts(ts, title=f"{self.name} - {ts_name}")
-        fig.show()
         fig = vis.plot_time_series_with_rolling_stats(ts, k=50)
-        fig.show()
         fig = vis.plot_annual_season_boxplots(ts)
-        fig.show()
+
+        # seasonality must be odd because of STL (loess smoothing must be centered)
+        # TODO parameterize this: expected_period, expected_seasonality as constructor args?
+        fig = vis.plot_ts_decomposition(ts, period=252, seasonality=251, stl=True)
+        fig = vis.plot_ts_decomposition(ts, period=252, seasonality=251, stl=False)
+
         fig = vis.plot_acf_pacf(ts)
-        fig.show()
         fig = vis.plot_top_k_autocorr_lags(ts, k=5)
-        fig.show()
 
     def stationarity_tests(self) -> pd.DataFrame:
         return pd.DataFrame(
@@ -81,11 +83,16 @@ class TimeSeries(abc.ABC):
 
 
 class SMH(TimeSeries):
-    def __init__(self):
-        super().__init__(name="SMH")
+    def __init__(self, differencing_periods: int = 1):
+        super().__init__(name="SMH", differencing_periods=differencing_periods)
 
-    def load_data(self):
-        smh = pd.read_csv('../data/smh-2000-06-05_2024-08-23.csv', parse_dates=True, index_col=0)
+    def load_data(self, fraction: float = 0.8, left_side: bool = True):
+        smh = pd.read_csv(DATA_DIR / 'smh-2000-06-05_2024-08-23.csv', parse_dates=True, index_col=0)
+
+        # basically a train-test split
+        n_samples = int(smh.shape[0] * fraction)
+        smh = smh[:n_samples] if left_side else smh[(smh.shape[0] - n_samples):]
+
         smh.index = smh.index.to_period('D')
 
         self.original = smh["Close"]
@@ -104,19 +111,28 @@ class SMH(TimeSeries):
             that years aren't 365 days, more like ~250
         """
 
-        yearly_cardinality = self.original.groupby(self.original.index.year).nunique()
+        yearly_cardinality = []
+        for year in self.original.index.year.unique():
+            yearly_cardinality.append({
+                "year": year,
+                "cardinality": np.sum(self.original.index.year == year)
+            })
 
-        cardinality_df = yearly_cardinality.reset_index()
-        cardinality_df.columns = ['year', 'cardinality']
+        cardinality_df = pd.DataFrame.from_records(yearly_cardinality)
         return cardinality_df
 
 
 class SemiconductorSales(TimeSeries):
-    def __init__(self):
-        super().__init__(name="SemiconductorSales")
+    def __init__(self, differencing_periods: int = 1):
+        super().__init__(name="SemiconductorSales", differencing_periods=differencing_periods)
 
-    def load_data(self):
-        sales = pd.read_csv('../data/global-semiconductor-sales-2012-2024.csv', parse_dates=True, index_col=0)
+    def load_data(self, fraction: float = 0.8, left_side: bool = True):
+        sales = pd.read_csv(DATA_DIR / 'global-semiconductor-sales-2012-2024.csv', parse_dates=True, index_col=0)
+
+        # basically a train-test split
+        n_samples = int(sales.shape[0] * fraction)
+        sales = sales[:n_samples] if left_side else sales[(sales.shape[0] - n_samples):]
+
         sales.index = sales.index.to_period('M')
 
         self.original = sales["sales"]
