83e787700d1926aaa25321ff242a27f70918c00b
Pierluigi Marchioro <pierluigi.marchioro@moku.io>
2024-08-30 14:15:09 +0200

    Minor fixes and refactoring to visualization.py

--- src/visualization.py
+++ src/visualization.py
@@ -1,24 +1,19 @@
 import matplotlib.pyplot as plt
-import seaborn as sns
 import numpy as np
 import pandas as pd
 import pandas.plotting as pd_plt
-import pmdarima as pm
-import pmdarima.model_selection as pm_modsel
-import statsmodels.tsa.stattools as tsa
+import seaborn as sns
 import statsmodels.graphics.tsaplots as tsa_plt
-import statsmodels.tsa.arima_model as arima
 import statsmodels.tsa.seasonal as tsa_season
-import sklearn.model_selection as sk_modsel
-import sklearn.metrics as sk_metrics
-from statsmodels.tsa.statespace.sarimax import SARIMAX
-from statsmodels.tsa.forecasting.stl import STLForecast, STLForecastResults
+import statsmodels.tsa.stattools as tsa
+
+DEFAULT_FIG_SIZE = (12, 8)
 
 
 def plot_ts(ts: pd.Series, title: str = "Time series") -> plt.Figure:
-    fig = ts.plot(figsize=(16, 12))
-    fig.set_title(title)
-    return fig
+    ax = ts.plot(figsize=DEFAULT_FIG_SIZE)
+    ax.figure.suptitle(title)
+    return ax.figure
 
 
 def plot_time_series_with_rolling_stats(ts: pd.Series, k: int) -> plt.Figure:
@@ -32,19 +27,21 @@ def plot_time_series_with_rolling_stats(ts: pd.Series, k: int) -> plt.Figure:
 
     # Compute rolling statistics
     rolling_mean = ts.rolling(window=k).mean()
-    rolling_std = ts.rolling(window=k).std()
+    rolling_std = ts.rolling(window=k).var()
+
+    # Required else plot breaks
+    if isinstance(ts.index, pd.PeriodIndex):
+        ts = ts.copy()
+        ts.index = ts.index.to_timestamp()
 
     # Create the plot
-    fig, ax = plt.subplots(figsize=(16, 12))
+    fig, ax = plt.subplots(figsize=DEFAULT_FIG_SIZE)
     ax.set_title('Time Series with Rolling Mean and Std')
     ax.set_xlabel('Time')
     ax.set_ylabel('Value')
     sns.lineplot(ax=ax, data=ts, label='Time Series')
     sns.lineplot(ax=ax, data=rolling_mean, label=f'Rolling Mean (window={k})')
     sns.lineplot(ax=ax, data=rolling_std, label=f'Rolling Std (window={k})')
-
-    # Show the legend
-    fig.legend()
 
     return fig
 
@@ -64,22 +61,30 @@ def plot_ts_decomposition(
     :param stl: whether to go with STL or MA-based decomposition
     :return:
     """
+
+    if isinstance(ts.index, pd.PeriodIndex):
+        ts = ts.copy()
+        ts.index = ts.index.to_timestamp()
+
     if stl:
         stl = tsa_season.STL(
             ts,
             period=period,
-            seasonality=seasonality,
+            seasonal=seasonality,
         )
         res = stl.fit()
         fig = res.plot()
+        fig.suptitle(f"STL decomposition - Seasonality={seasonality}")
     else:
         fig = tsa_season.seasonal_decompose(
             ts, model='additive',
             period=seasonality
         ).plot()
+        fig.suptitle(f"MA-based decomposition - Seasonality={seasonality}")
 
-    fig.set_size_inches(16, 12)
+    fig.set_size_inches(DEFAULT_FIG_SIZE)
     return fig
+
 
 def plot_annual_season_boxplots(ts: pd.Series) -> plt.Figure:
     seasons_data = {}
@@ -96,7 +101,7 @@ def plot_annual_season_boxplots(ts: pd.Series) -> plt.Figure:
         seasons_data[month_label] = ts[ts.index.month == month].values
 
     # Plot the boxplot for each season
-    fig, ax = plt.subplots(figsize=(16, 12))
+    fig, ax = plt.subplots(figsize=DEFAULT_FIG_SIZE)
     sns.boxplot(ax=ax, data=seasons_data)
 
     # Adding title and labels
@@ -108,34 +113,42 @@ def plot_annual_season_boxplots(ts: pd.Series) -> plt.Figure:
 
 
 def plot_acf_pacf(ts: pd.Series) -> plt.Figure:
-    fig, axs = plt.subplots(nrows=1, ncols=3, figsize=(16, 24))
+    fig, axs = plt.subplots(nrows=3, ncols=1, figsize=(16, 24))
+
+    # This first plot should be a little bit more zoomed out
+    # I saw a heuristic on some StackExchange discussions that said to plot lags
+    #   until time_series.length/100 or until the ACF goes to 0, whichever of the two comes first
     tsa_plt.plot_acf(ts, ax=axs[0], lags=ts.shape[0] // 100, auto_ylims=True)
-    tsa_plt.plot_acf(ts, ax=axs[0], auto_ylims=True)
-    tsa_plt.plot_pacf(ts, ax=axs[0], auto_ylims=True)
+
+    # These two are automatically zoomed-in by the plotting library, based on the behavior of ACF values
+    tsa_plt.plot_acf(ts, ax=axs[1], auto_ylims=True)
+    tsa_plt.plot_pacf(ts, ax=axs[2], auto_ylims=True)
 
     return fig
 
 
 def plot_top_k_autocorr_lags(ts: pd.Series, k: int = 10) -> plt.Figure:
     # Extract the top k autocorr scores
-    acf_res, _ = tsa.acf(ts, nlags=40, alpha=0.05)
+    acf_res, _ = tsa.acf(ts, nlags=100, alpha=0.05)
 
+    # From 1 to k + 1 because lag 0 is not interesting
+    top_lags = np.argsort(-np.abs(acf_res))[1:k+1]
     top_autocorr = pd.DataFrame({
-        # k + 1 because lag 0 is not really interesting
-        "lag": np.argsort(-np.abs(acf_res))[:k+1],
-        "autocorr": np.sort(-np.abs(acf_res))[:k+1]
+        "lag": top_lags,
+        "autocorr": acf_res[top_lags]
     })
 
-    # print(np.argsort(-np.abs(acf_res))[1:k+1])
-    # print(np.sort(-np.abs(acf_res))[1:11])
-
-    # Plot every lag except 0
+    # Plot every lag except 0 - also, the first chart of the grid is the autocorr barplot
     to_plot = top_autocorr["lag"]
     nrows, ncols = len(to_plot) // 2 + 1, 2
     fig, axs = plt.subplots(nrows, ncols, figsize=(ncols*10, nrows*5))
     fig.suptitle(f"Top {k} autocorrelation lags")
 
-    for i, lag in enumerate(to_plot[1:]):
+    # Barplot of the top autocorrelations
+    sns.barplot(ax=axs[0, 0], data=top_autocorr, x="lag", y="autocorr", color="skyblue")
+
+    for i, lag in enumerate(to_plot):
+        i += 1  # because first axes is occupied by the top-autocorr chart
         pd_plt.lag_plot(ts, ax=axs[i // ncols, i % ncols], lag=lag)
 
     return fig
